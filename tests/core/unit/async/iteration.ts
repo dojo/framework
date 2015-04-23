8bac0dc5401f7b1cc78bc1a7baa42c20a067cd2b
Paul Shannon <pshannon@sitepen.com>
2015-04-22 17:29:12 -0700

    async/iteration.every() implementation

---
+++ tests/core/unit/async/iteration.ts
@@ -0,0 +1,7 @@
+import registerSuite = require('intern!object');
+import assert = require('intern/chai!assert');
+import iteration = require('src/async/iteration');
+
+registerSuite({
+	name: 'async/iteration'
+});
