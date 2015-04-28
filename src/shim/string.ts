0484915c645030d9059ec305872445e7b6387055
Matt Wistrand <matt@troglodytidae.com>
2015-04-28 09:31:07 -0500

    Implement string escape methods and unit tests.

--- src/shim/string.ts
+++ src/shim/string.ts
@@ -1,3 +1,14 @@
+var escapeRegExpPattern = /[[\]{}()|\\^$.*+?]/g;
+var escapeXmlPattern = /[&<]/g;
+var escapeXmlForPattern = /[&<>'"]/g;
+var escapeXmlMap: { [key: string]: string } = {
+	'&': '&amp;',
+	'<': '&lt;',
+	'>': '&gt;',
+	'"': '&quot;',
+	'\'': '&#39;'
+};
+
 function getPadding(text: string, size: number, character: string = '0'): string {
 	var length: number = Math.abs(Math.min(0, text.length - size));
 	var padding: string = '';
@@ -22,6 +33,22 @@ function getPadding(text: string, size: number, character: string = '0'): string
 	}
 
 	return padding;
+}
+
+export function escapeRegExp(text: string): string {
+	return !text ? text : text.replace(escapeRegExpPattern, '\\$&');
+}
+
+export function escapeXml(xml: string, forAttribute: boolean = true): string {
+	if (!xml) {
+		return xml;
+	}
+
+	var pattern = forAttribute ? escapeXmlForPattern : escapeXmlPattern;
+
+	return xml.replace(pattern, function (character: string): string {
+		return escapeXmlMap[character];
+	});
 }
 
 export function padStart(text: string, size: number, character: string = '0'): string {
