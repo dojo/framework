d3c722dd19ddb0039b43fdb372d044b5f21c151c
Matt Wistrand <matt@troglodytidae.com>
2015-04-30 16:41:50 -0500

    Add basic benchmarks for lang and string.
    * Add Benchmark.js as a dev dependency.
    * Add TS definition for Benchmark.js.

---
+++ tests/core/benchmark/lang.ts
@@ -0,0 +1,224 @@
+import Benchmark = require('benchmark');
+import lang = require('../../src/lang');
+
+var benchmarks: Benchmark[] = [];
+
+function onComplete() {
+	console.log(this.name + ': ' + this.hz + ' with a margin of error of ' + this.stats.moe);
+}
+
+benchmarks.push(new Benchmark('lang.copy (single source, all options false)',
+	// The `setup` option cannot be used, as the TypeScript compiler does not know
+	// that `setup`'s local variables are made available to the test function.
+	(function () {
+		var options = {
+			sources: [ <any> { a: 1, b: 'Lorem ipsum', c: [], d: 4 } ]
+		};
+
+		return function () {
+			lang.copy(options);
+		};
+	})(), {
+		onComplete: onComplete
+	}));
+
+benchmarks.push(new Benchmark('lang.copy (single source, descriptors true)', (function () {
+	var options = {
+		descriptors: true,
+		sources: [
+			Object.create(Object.prototype, {
+				a: {
+					value: 1,
+					enumerable: true,
+					configurable: true,
+					writable: true
+				},
+				b: {
+					value: 'Lorem ipsum',
+					enumerable: true,
+					configurable: true,
+					writable: true
+				},
+				c: {
+					value: [],
+					enumerable: true,
+					configurable: true,
+					writable: true
+				},
+				d: {
+					value: 4,
+					enumerable: true,
+					configurable: true,
+					writable: true
+				}
+			})
+		]
+	};
+
+	return function () {
+		lang.copy(options);
+	};
+})(), {
+	onComplete: onComplete
+}));
+
+benchmarks.push(new Benchmark('lang.copy (multiple sources, all options true)', (function () {
+	var options = {
+		assignPrototype: true,
+		deep: true,
+		descriptors: true,
+		sources: [
+			Object.create(Object.create(null, {
+				x: {
+					value: ('1234567890').split('')
+				},
+				y: {
+					enumerable: true,
+					value: /\s/
+				}
+			}), {
+				a: {
+					value: 1,
+					enumerable: true,
+					configurable: true,
+					writable: true
+				},
+				b: {
+					value: 'Lorem ipsum',
+					enumerable: true,
+					configurable: true,
+					writable: true
+				},
+				c: {
+					value: [],
+					enumerable: true,
+					configurable: true,
+					writable: true
+				},
+				d: {
+					value: 4,
+					enumerable: true,
+					configurable: true,
+					writable: true
+				}
+			}),
+
+			{
+				b: 'Dolor sit amet.',
+				c: [ 1, 2, 3 ],
+				d: 5
+			},
+
+			(function () {
+				function Answers(kwArgs: { [key: string]: any }) {
+					Object.keys(kwArgs).forEach(function (key: string): void {
+						(<any> this)[key] = kwArgs[key];
+					}, this);
+				}
+
+				return new (<any> Answers)({
+					universe: 42
+				});
+			})()
+		]
+	};
+
+	return function () {
+		lang.copy(options);
+	};
+})(), {
+	onComplete: onComplete
+}));
+
+benchmarks.push(new Benchmark('lang.create', (function () {
+	var object = <any> { a: 1, b: 'Lorem ipsum', c: [], d: 4 };
+
+	return function () {
+		lang.create(object, object, object, object);
+	};
+})(), {
+	onComplete: onComplete
+}));
+
+benchmarks.push(new Benchmark('lang.duplicate', (function () {
+	var object = <any> { a: 1, b: 'Lorem ipsum', c: [], d: 4 };
+
+	return function () {
+		lang.duplicate(object);
+	};
+})(), {
+	onComplete: onComplete
+}));
+
+benchmarks.push(new Benchmark('lang.getPropertyDescriptor', (function () {
+	var prototype = <any> { a: 1, b: 'Lorem ipsum', c: [], d: 4 };
+	var object = prototype;
+	var i = 10;
+
+	while (i > 0) {
+		object = Object.create(object);
+		--i;
+	}
+
+	Object.defineProperty(prototype, 'e', {
+		value: 5,
+		configurable: false,
+		enumerable: true,
+		writable: false
+	});
+
+	return function () {
+		lang.getPropertyDescriptor(object, 'e');
+	};
+})(), {
+	onComplete: onComplete
+}));
+
+benchmarks.push(new Benchmark('lang.isIdentical', (function () {
+	var a = Number('asdfx{}');
+	var b = Number('xkcd');
+
+	return function () {
+		lang.isIdentical(a, b);
+	};
+})(), {
+	onComplete: onComplete
+}));
+
+benchmarks.push(new Benchmark('lang.lateBind', (function () {
+	var object: any = {
+		method: function () {}
+	};
+
+	return function () {
+		lang.lateBind(object, 'method');
+	};
+})(), {
+	onComplete: onComplete
+}));
+
+benchmarks.push(new Benchmark('lang.lateBind (partial application)', (function () {
+	var object: any = {
+		method: function () {}
+	};
+
+	return function () {
+		lang.lateBind(object, 'method', 1, 2, 3);
+	};
+})(), {
+	onComplete: onComplete
+}));
+
+benchmarks.push(new Benchmark('lang.partial', (function () {
+	function f() {}
+
+	return function () {
+		lang.partial(f, 1, 2, 3);
+	};
+})(), {
+	onComplete: onComplete
+}));
+
+benchmarks.forEach(function (benchmark: Benchmark): void {
+	benchmark.run();
+});
---
+++ tests/core/benchmark/string.ts
@@ -0,0 +1,30 @@
+var Benchmark = require('benchmark');
+import {padEnd, padStart} from '../../src/string';
+
+var text: string = 'Lorem';
+var count: number = 10;
+var character: string = ' ';
+
+var benchmarks: any[] = [];
+
+benchmarks.push({
+	name: 'stringUtil.padStart',
+	fn: function () {
+		padStart(text, count, character);
+	}
+});
+
+benchmarks.push({
+	name: 'stringUtil.padEnd',
+	fn: function () {
+		padEnd(text, count, character);
+	}
+});
+
+benchmarks.forEach(function (benchmark: any): void {
+	new Benchmark(benchmark.name, benchmark.fn, {
+		onComplete: function () {
+			console.log(this.name + ': ' + this.hz + ' with a margin of error of ' + this.stats.moe);
+		}
+	}).run();
+});
