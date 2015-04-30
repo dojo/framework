d0481d7ef029f600247fa4220a5791f2911d3780
Paul Shannon <pshannon@sitepen.com>
2015-04-30 12:12:03 -0700

    adding comments to delay and timeout

--- src/core/async/delay.ts
+++ src/core/async/delay.ts
@@ -1,5 +1,11 @@
 import Promise from './Promise';
 
+/**
+ * Used for delaying a Promise chain for a specific number of milliseconds.
+ *
+ * @param milliseconds the number of milliseconds to delay
+ * @return {function(T): Promise<T>} a function producing a promise that eventually returns the value passed to it; usable with Thenable.then()
+ */
 export default function delay<T>(milliseconds: number): Identity<T> {
 	return function (value: T): Promise<T> {
 		return new Promise(function (resolve) {
