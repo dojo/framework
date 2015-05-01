ac645725d6e3fa1fce581670c8ba3cabaf9b22b5
Mangala SSS Khalsa <mkhalsa@sitepen.com>
2015-05-01 12:10:26 -0700

    TransformStream: add async transform unit test

--- tests/shim/unit/streams/TransformStream.ts
+++ tests/shim/unit/streams/TransformStream.ts
@@ -1,5 +1,6 @@
 import * as registerSuite from 'intern!object';
 import * as assert from 'intern/chai!assert';
+import Promise from 'src/Promise';
 import { ReadResult, Transform } from 'src/streams/interfaces';
 import TransformStream from 'src/streams/TransformStream';
 
@@ -15,6 +16,7 @@ registerSuite({
 			},
 
 			flush(enqueue: Function, close: Function): void {
+				close();
 				return;
 			},
 
@@ -40,6 +42,66 @@ registerSuite({
 			return reader.read().then((result: ReadResult<number>) => {
 				assert.strictEqual(result.value, testValue.charCodeAt(0));
 			});
+		});
+	},
+
+	'async transform'() {
+		var testValues = ['a', 'b', 'c'];
+		var input = [].concat(testValues);
+		var transform: Transform<number, string> = {
+			transform(chunk: string, enqueueInReadable: (chunk: number) => void, transformDone: () => void): void {
+				setTimeout(() => {
+					enqueueInReadable(chunk.charCodeAt(0));
+					transformDone();
+				}, 20);
+			},
+
+			flush(enqueue: Function, close: Function): void {
+				close();
+				return;
+			},
+
+			readableStrategy: {
+				size(chunk: number) {
+					return 1;
+				},
+				highwaterMark: Infinity
+			},
+
+			writableStrategy: {
+				size(chunk: string) {
+					return 1;
+				},
+				highwaterMark: Infinity
+			}
+		};
+
+		var stream = new TransformStream(transform);
+		var reader = stream.readable.getReader();
+		var results: number[] = [];
+
+		while (input.length) {
+			stream.writable.write(input.shift());
+		}
+
+		stream.writable.close();
+
+		function readNext(): Promise<void> {
+			return reader.read().then((result: ReadResult<number>) => {
+				if (result.done) {
+					return Promise.resolve();
+				}
+				else {
+					results.push(result.value);
+					return readNext();
+				}
+			});
+		}
+
+		return readNext().then(() => {
+			for (let i = 0; i < results.length; i++) {
+				assert.strictEqual(results[i], testValues[i].charCodeAt(0));
+			}
 		});
 	}
 });
