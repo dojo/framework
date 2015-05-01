bf9733c7007ec6ed14142ae366b380f72d6c16b3
Mangala SSS Khalsa <mkhalsa@sitepen.com>
2015-04-30 20:13:27 -0700

    TransformStream: add simple transform test

--- tests/core/unit/streams/TransformStream.ts
+++ tests/core/unit/streams/TransformStream.ts
@@ -1,12 +1,45 @@
 import * as registerSuite from 'intern!object';
 import * as assert from 'intern/chai!assert';
-
+import { ReadResult, Transform } from 'src/streams/interfaces';
 import TransformStream from 'src/streams/TransformStream';
 
 registerSuite({
 	name: 'TransformStream',
 
-	'test test'() {
-		assert.isTrue(true);
+	'simple transform'() {
+		var testValue = 'a';
+		var transform: Transform<number, string> = {
+			transform(chunk: string, enqueueInReadable: (chunk: number) => void, transformDone: () => void): void {
+				enqueueInReadable(chunk.charCodeAt(0));
+				transformDone();
+			},
+
+			flush(enqueue: Function, close: Function): void {
+				return;
+			},
+
+			readableStrategy: {
+				size(chunk: number) {
+					return 1;
+				},
+				highwaterMark: Infinity
+			},
+
+			writableStrategy: {
+				size(chunk: string) {
+					return 1;
+				},
+				highwaterMark: Infinity
+			}
+		};
+
+		var stream = new TransformStream(transform);
+		var reader = stream.readable.getReader();
+
+		return stream.writable.write(testValue).then(() => {
+			return reader.read().then((result: ReadResult<number>) => {
+				assert.strictEqual(result.value, testValue.charCodeAt(0));
+			});
+		});
 	}
 });
