ba03739a78e33fc5291037ff66af724e7bc3d702
Mangala SSS Khalsa <mkhalsa@sitepen.com>
2015-05-03 17:08:43 -0700

    ManualSink: create module

---
+++ tests/shim/unit/streams/helpers/ManualSink.ts
@@ -0,0 +1,39 @@
+import { Sink } from 'src/streams/interfaces';
+import Promise from 'src/Promise';
+
+// A sink whose write operations must be manually resolved by calling 'next'
+export default class ManualSink<T> implements Sink<T> {
+	values: Array<T> = [];
+	protected _resolvers: Function[] = [];
+
+	write(chunk: T): Promise<void> {
+		this.values.push(chunk);
+
+		return new Promise<void>((resolve, reject) => {
+			this._resolvers.push(resolve);
+		});
+	}
+
+	next() {
+		if (!this._resolvers.length) {
+			console.warn('ManualSink.next called with nothing in queue');
+			return;
+		}
+
+		var resolve = this._resolvers.shift();
+
+		resolve();
+	}
+
+	abort(reason: any): Promise<void> {
+		return Promise.resolve();
+	}
+
+	close(): Promise<void> {
+		return Promise.resolve();
+	}
+
+	start(): Promise<void> {
+		return Promise.resolve();
+	}
+}
