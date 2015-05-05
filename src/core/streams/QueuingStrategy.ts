14bbed81ae8e7aedc5a5b642de9a7c439d4e7648
Ed Hager <ehager@sitepen.com>
2015-05-05 15:44:05 -0500

    Remove the module definition from around the KwArgs interface in QueuingStrategy.

--- src/core/streams/QueuingStrategy.ts
+++ src/core/streams/QueuingStrategy.ts
@@ -3,13 +3,11 @@ import { Strategy } from './interfaces';
 export default class QueuingStrategy<T> implements Strategy<T> {
 	highWaterMark: number;
 
-	constructor(kwArgs: QueuingStrategy.KwArgs) {
+	constructor(kwArgs: KwArgs) {
 		this.highWaterMark = kwArgs.highWaterMark;
 	}
 }
 
-module QueuingStrategy {
-	export interface KwArgs {
-		highWaterMark: number;
-	}
+export interface KwArgs {
+	highWaterMark: number;
 }
