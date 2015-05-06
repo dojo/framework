83f068ca612b39222ae8ea66616b62a58b1e25e6
Matt Wistrand <matt@troglodytidae.com>
2015-05-06 16:52:57 -0500

    WIP Incorporate Object.observe in ObjectObserver.

--- src/shim/observers/interfaces.ts
+++ src/shim/observers/interfaces.ts
@@ -1,7 +1,8 @@
 import core = require('../interfaces');
 
-export interface ObjectObserver extends core.Handle {
+export interface Observer extends core.Handle {
 	observeProperty(...property: string[]): void;
+	removeProperty(...property: string[]): void;
     nextTurn?: boolean;
     onlyReportObserved: boolean;
 }
