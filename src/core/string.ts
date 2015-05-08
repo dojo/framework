30998370af175db200c2ebaa63d84ed2e3d55459
Kenneth G. Franqueiro <kfranqueiro@sitepen.com>
2015-05-08 16:59:57 -0400

    string: add startsWith, endsWith, and includes

--- src/core/string.ts
+++ src/core/string.ts
@@ -35,6 +35,41 @@ function getPadding(text: string, size: number, character: string = '0'): string
 	return padding;
 }
 
+/**
+ * Validates that text is defined, and normalizes position (based on the given default if the input is NaN).
+ * Used by startsWith, includes, and endsWith.
+ * @return Normalized position.
+ */
+function normalizeSubstringArgs(name: string, text: string, search: string, position: number,
+		isEnd: boolean = false): [ string, string, number ] {
+	if (text == null) {
+		throw new TypeError('string.' + name + ' requires a valid string to search against.');
+	}
+
+	let length = text.length;
+	position = position !== position ? (isEnd ? length : 0) : position;
+	return [ text, String(search), Math.min(Math.max(position, 0), length) ];
+}
+
+/**
+ * Determines whether a string ends with the given substring.
+ * @return Boolean indicating if the search string was found at the end of the given string
+ */
+export function endsWith(text: string, search: string, endPosition?: number): boolean {
+	if (endPosition == null) {
+		endPosition = text.length;
+	}
+
+	[ text, search, endPosition ] = normalizeSubstringArgs('endsWith', text, search, endPosition, true);
+
+	let start = endPosition - search.length;
+	if (start < 0) {
+		return false;
+	}
+
+	return text.slice(start, endPosition) === search;
+}
+
 export function escapeRegExp(text: string): string {
 	return !text ? text : text.replace(escapeRegExpPattern, '\\$&');
 }
@@ -49,6 +84,15 @@ export function escapeXml(xml: string, forAttribute: boolean = true): string {
 	return xml.replace(pattern, function (character: string): string {
 		return escapeXmlMap[character];
 	});
+}
+
+/**
+ * Determines whether a string includes the given substring (optionally starting from a given index).
+ * @return Boolean indicating if the search string was found within the given string
+ */
+export function includes(text: string, search: string, position: number = 0): boolean {
+	[ text, search, position ] = normalizeSubstringArgs('includes', text, search, position);
+	return text.indexOf(search, position) !== -1;
 }
 
 export function padStart(text: string, size: number, character: string = '0'): string {
@@ -73,4 +117,20 @@ export function padEnd(text: string, size: number, character: string = '0'): str
 	}
 
 	return text + getPadding(text, size, character);
+}
+
+/**
+ * Determines whether a string begins with the given substring (optionally starting from a given index).
+ * @return Boolean indicating if the search string was found at the beginning of the given string
+ */
+export function startsWith(text: string, search: string, position: number = 0): boolean {
+	search = String(search);
+	[ text, search, position ] = normalizeSubstringArgs('startsWith', text, search, position);
+
+	let end = position + search.length;
+	if (end > text.length) {
+		return false;
+	}
+
+	return text.slice(position, end) === search;
 }
