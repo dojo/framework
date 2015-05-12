43e36bf9ea7c72982b59911041bc0293487672dc
Matt Wistrand <matt@troglodytidae.com>
2015-05-12 16:13:07 -0500

    Simplify Scheduler.
    * Remove ID checking and deduping.
    * Make necessary changes in order to conform with the style guide.

--- src/core/Scheduler.ts
+++ src/core/Scheduler.ts
@@ -1,13 +1,13 @@
 import { Handle } from './interfaces';
-import { QueueItem, queueTask, queueAnimationTask, queueMicroTask } from './queue';
+import { queueAnimationTask, QueueItem, queueMicroTask, queueTask } from './queue';
 
-let typeMap: { [key: string]: (callback: (...args: any[]) => any) => Handle; } = {
-	'macro': queueTask,
-	'micro': queueMicroTask,
-	'animation': queueAnimationTask
+const typeMap: { [key: string]: (callback: (...args: any[]) => any) => Handle; } = {
+	animation: queueAnimationTask,
+	macro: queueTask,
+	micro: queueMicroTask
 };
 
-function getQueueHandle(item: Item): Handle {
+function getQueueHandle(item: QueueItem): Handle {
 	return {
 		destroy: function () {
 			this.destroy = function () {};
@@ -22,15 +22,16 @@ export interface KwArgs {
 	type?: string;
 }
 
-export interface Item extends QueueItem {
-	id?: string;
-}
-
 export default class Scheduler {
+	protected _boundDispatch: () => void;
+	protected _deferred: QueueItem[];
+	protected _isProcessing: boolean;
+	protected _queue: QueueItem[];
+	protected _task: Handle;
+
 	/**
 	 * Determines whether any callbacks registered during should be added to the current batch (`false`)
-	 * or deferred until the next batch (`true`, default). If this is set to `false`, then any IDs passed
-	 * to callbacks registered by other callbacks in the batch will be ignored.
+	 * or deferred until the next batch (`true`, default).
 	 */
 	deferWhileProcessing: boolean;
 
@@ -40,41 +41,8 @@ export default class Scheduler {
 	 */
 	type: string;
 
-	protected _boundDispatch: () => void;
-	protected _deferred: Item[];
-	protected _idMap: { [key: string]: number };
-	protected _isProcessing: boolean;
-	protected _queue: Item[];
-	protected _task: Handle;
-
-	constructor(kwArgs?: KwArgs) {
-		this.deferWhileProcessing = (kwArgs && 'deferWhileProcessing' in kwArgs) ? kwArgs.deferWhileProcessing : true;
-		this.type = (kwArgs && kwArgs.type && kwArgs.type in typeMap) ? kwArgs.type : 'macro';
-
-		this._boundDispatch = this._dispatch.bind(this);
-		this._isProcessing = false;
-		this._queue = [];
-	}
-
-	schedule(callback: (...args: any[]) => void, id?: string): Handle {
-		if (this._isProcessing && this.deferWhileProcessing) {
-			return this._defer(callback, id);
-		}
-
-		let item: Item = {
-			id: id,
-			isActive: true,
-			callback: callback
-		};
-
-		this._schedule(item);
-
-		return getQueueHandle(item);
-	}
-
-	protected _defer(callback: (...args: any[]) => void, id?: string): Handle {
-		let item: Item = {
-			id: id,
+	protected _defer(callback: (...args: any[]) => void): Handle {
+		const item: QueueItem = {
 			isActive: true,
 			callback: callback
 		};
@@ -92,10 +60,9 @@ export default class Scheduler {
 		this._isProcessing = true;
 		this._task.destroy();
 		this._task = null;
-		this._idMap = null;
 
-		let queue = this._queue;
-		let item: Item;
+		const queue = this._queue;
+		let item: QueueItem;
 
 		while (item = queue.shift()) {
 			if (item.isActive) {
@@ -105,42 +72,53 @@ export default class Scheduler {
 
 		this._isProcessing = false;
 
-		let deferred: Item[] = this._deferred;
+		let deferred: QueueItem[] = this._deferred;
 		if (deferred && deferred.length) {
 			this._deferred = null;
 
-			let item: Item;
+			let item: QueueItem;
 			while (item = deferred.shift()) {
 				this._schedule(item);
 			}
 		}
 	}
 
-	protected _schedule(item: Item): void {
-		let queue = this._queue;
-		let idMap = this._idMap;
-		let id: string = item.id;
-
+	protected _schedule(item: QueueItem): void {
 		if (!this._task) {
 			this._task = typeMap[this.type](this._boundDispatch);
 		}
 
-		// Specifying an ID only makes sense when the queue is still being built. Once the loop
-		// has begun execution, anything that might normally be replaced will have already been executed.
-		if (id && !this._isProcessing) {
-			if (!idMap) {
-				idMap = this._idMap = {};
-			}
+		this._queue.push(item);
+	}
 
-			if (id in idMap) {
-				queue.splice(idMap[id], 1);
-			}
+	constructor(kwArgs?: Scheduler.KwArgs) {
+		this.deferWhileProcessing = (kwArgs && 'deferWhileProcessing' in kwArgs) ? kwArgs.deferWhileProcessing : true;
+		this.type = (kwArgs && kwArgs.type && kwArgs.type in typeMap) ? kwArgs.type : 'macro';
+
+		this._boundDispatch = this._dispatch.bind(this);
+		this._isProcessing = false;
+		this._queue = [];
+	}
+
+	schedule(callback: (...args: any[]) => void): Handle {
+		if (this._isProcessing && this.deferWhileProcessing) {
+			return this._defer(callback);
 		}
 
-		queue.push(item);
+		const item: QueueItem = {
+			isActive: true,
+			callback: callback
+		};
 
-		if (id && !this._isProcessing) {
-			idMap[id] = queue.length - 1;
-		}
+		this._schedule(item);
+
+		return getQueueHandle(item);
+	}
+}
+
+module Scheduler {
+	export interface KwArgs {
+		deferWhileProcessing?: boolean;
+		type?: string;
 	}
 }
