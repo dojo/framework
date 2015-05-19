75aed0caedd593c39a0fd289896ce1a697786c98
James Donaghue <james.donaghue@gmail.com>
2015-05-19 13:48:34 -0400

    Implement encoding module and tests

--- src/core/string.ts
+++ src/core/string.ts
@@ -10,6 +10,10 @@ const escapeXmlMap: Hash<string> = {
 	'"': '&quot;',
 	'\'': '&#39;'
 };
+export const HIGH_SURROGATE_MIN = 0xD800;
+export const HIGH_SURROGATE_MAX = 0xDBFF;
+export const LOW_SURROGATE_MIN = 0xDC00;
+export const LOW_SURROGATE_MAX = 0xDFFF;
 
 /**
  * Performs validation and padding operations used by padStart and padEnd.
@@ -69,12 +73,12 @@ export function codePointAt(text: string, position: number = 0) {
 
 	// Get the first code unit
 	const first = text.charCodeAt(position);
-	if (first >= 0xD800 && first <= 0xDBFF && length > position + 1) {
+	if (first >= HIGH_SURROGATE_MIN && first <= HIGH_SURROGATE_MAX && length > position + 1) {
 		// Start of a surrogate pair (high surrogate and there is a next code unit); check for low surrogate
 		// https://mathiasbynens.be/notes/javascript-encoding#surrogate-formulae
 		const second = text.charCodeAt(position + 1);
-		if (second >= 0xDC00 && second <= 0xDFFF) {
-			return (first - 0xD800) * 0x400 + second - 0xDC00 + 0x10000;
+		if (second >= LOW_SURROGATE_MIN && second <= LOW_SURROGATE_MAX) {
+			return (first - HIGH_SURROGATE_MIN) * 0x400 + second - LOW_SURROGATE_MIN + 0x10000;
 		}
 	}
 	return first;
@@ -165,8 +169,8 @@ export function fromCodePoint(...codePoints: number[]): string {
 			// Astral code point; split in surrogate halves
 			// https://mathiasbynens.be/notes/javascript-encoding#surrogate-formulae
 			codePoint -= 0x10000;
-			let highSurrogate = (codePoint >> 10) + 0xD800;
-			let lowSurrogate = (codePoint % 0x400) + 0xDC00;
+			let highSurrogate = (codePoint >> 10) + HIGH_SURROGATE_MIN;
+			let lowSurrogate = (codePoint % 0x400) + LOW_SURROGATE_MIN;
 			codeUnits.push(highSurrogate, lowSurrogate);
 		}
 
