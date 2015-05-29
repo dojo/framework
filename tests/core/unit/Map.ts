90b970daf35ee96e9872d6f8e6b55b78a9a8f8d0
Kenneth G. Franqueiro <kfranqueiro@sitepen.com>
2015-05-29 15:08:30 -0400

    Map cleanup
    * Tighten typing for constructor argument
    * private->protected for extensibility
    * Remove entries, keys, and values APIs
    * Tighten forEach code and fix its callback's argument order
    * Replace var with let/const
    * JSDoc tweaks
    * Add a few more tests for e.g. NaN and size updates
    * Reorder tests mostly-alphabetically (except instantiation)
    
    Resolves #8.

--- tests/core/unit/Map.ts
+++ tests/core/unit/Map.ts
@@ -27,13 +27,121 @@ registerSuite({
 			});
 		},
 
-		'array like data'() {
+		'array-like data'() {
 			assert.doesNotThrow(function () {
 				map = new Map<number, string>({
 					length: 1,
 					0: [ 3, 'bar' ]
 				});
 			});
+		}
+	},
+
+	clear: {
+		'empty map'() {
+			map = new Map<void, void>();
+			assert.doesNotThrow(function () {
+				map.clear();
+			});
+		},
+
+		'non-empty map'() {
+			map = new Map<number, string>([
+				[ 3, 'abc' ]
+			]);
+			map.clear();
+			assert.isFalse(map.has(3));
+			assert.strictEqual(map.size, 0);
+		}
+	},
+
+	'delete': {
+		before() {
+			map = new Map<number, string>([
+				[ 3, 'abc' ],
+				[ 4, 'def' ]
+			]);
+		},
+
+		'key found'() {
+			assert.isTrue(map.delete(3));
+			assert.isUndefined(map.get(3));
+			assert.strictEqual(map.size, 1);
+			assert.strictEqual(map.get(4), 'def', 'Remaining key should not be affected by delete');
+		},
+
+		'key not found'() {
+			assert.isFalse(map.delete('foo'));
+		}
+	},
+
+	forEach: {
+		before() {
+			function foo() {}
+			const object = Object.create(null);
+			const array: any[] = [];
+			mapArgs = [
+				[ 0, 0 ],
+				[ 1, '1' ],
+				[ 2, object ],
+				[ 3, array ],
+				[ 4, foo ],
+				[ 5, undefined ]
+			];
+			map = new Map<number, any>(mapArgs);
+		},
+
+		'callback arguments'() {
+			map.forEach(function (value, key, mapInstance) {
+				assert.lengthOf(arguments, 3);
+				assert.strictEqual(map.get(key), value);
+				assert.strictEqual(map, mapInstance);
+			});
+		},
+
+		'times executed'() {
+			let counter = 0;
+			map.forEach(function (key, value, mapInstance) {
+				counter++;
+			});
+			assert.strictEqual(counter, mapArgs.length);
+		}
+	},
+
+	get: {
+		before() {
+			map = new Map<number, string>([
+				[ 0, 'a' ],
+				[ 8, 'b' ],
+				[ NaN, 'c' ]
+			]);
+		},
+
+		'key found'() {
+			assert.strictEqual(map.get(0), 'a');
+			assert.strictEqual(map.get(8), 'b');
+			assert.strictEqual(map.get(NaN), 'c',
+				'Map should successfully retrieve an item with a key of NaN');
+		},
+
+		'key not found'() {
+			assert.isUndefined(map.get(3));
+		}
+	},
+
+	has: {
+		before() {
+			map = new Map<number, string>([
+				[ 3, 'abc' ]
+			]);
+		},
+
+		'key found'() {
+			assert.isTrue(map.has(3));
+		},
+
+		'key not found'() {
+			assert.isFalse(map.has(0));
 		}
 	},
 
@@ -78,7 +186,7 @@ registerSuite({
 		},
 
 		'key exists'() {
-			map = new Map<number, string>([[ 3, 'abc' ]]);
+			map = new Map<number, string>([ [ 3, 'abc' ] ]);
 			map.set(3, 'def');
 			assert.strictEqual(map.get(3), 'def');
 		},
@@ -86,170 +194,14 @@ registerSuite({
 		'size updates'() {
 			map = new Map<string, string>();
 			assert.strictEqual(map.size, 0);
+
 			map.set('foo', 'bar');
-			assert.strictEqual(map.size, 1);
-		}
-	},
+			assert.strictEqual(map.size, 1,
+				'size should increase after setting a new key');
 
-	get: {
-		before() {
-			map = new Map<number, string>([
-				[0, 'a'],
-				[8, 'b']
-			]);
-		},
-
-		'key found'() {
-			assert.strictEqual(map.get(0), 'a');
-			assert.strictEqual(map.get(8), 'b');
-		},
-
-		'key not found'() {
-			assert.isUndefined(map.get(3));
-		}
-	},
-
-	'delete': {
-		before() {
-			map = new Map<number, string>([
-				[ 3, 'abc' ]
-			]);
-		},
-
-		'key found'() {
-			assert.isTrue(map.delete(3));
-			assert.isUndefined(map.get(3));
-		},
-
-		'key not found'() {
-			assert.isFalse(map.delete('foo'));
-		}
-	},
-
-	has: {
-		before() {
-			map = new Map<number, string>([
-				[ 3, 'abc' ]
-			]);
-		},
-
-		'key found'() {
-			assert.isTrue(map.has(3));
-		},
-
-		'key not found'() {
-			assert.isFalse(map.has(0));
-		}
-	},
-
-	clear: {
-		'empty map'() {
-			map = new Map<void, void>();
-			assert.doesNotThrow(function () {
-				map.clear();
-			});
-		},
-
-		'nonempty map'() {
-			map = new Map<number, string>([
-				[ 3, 'abc' ]
-			]);
-			map.clear();
-			assert.isFalse(map.has(3));
-			assert.strictEqual(map.size, 0);
-		}
-	},
-
-	keys: {
-		'empty map'() {
-			map = new Map<void, void>();
-			assert.deepEqual(map.keys(), []);
-		},
-
-		'nonempty map'(){
-			function foo() { }
-			let object = Object.create(null);
-			let array = new Array();
-			map = new Map<any, any>([
-				[0, 0],
-				['1', 1],
-				[object, 2],
-				[array, 3],
-				[foo, 4]
-			]);
-			assert.deepEqual(map.keys(), [
-				0, '1', object, array, foo
-			]);
-		},
-	},
-
-	values() {
-		function foo() {}
-		let object = Object.create(null);
-		let array = new Array();
-		map = new Map<number, any>([
-			[ 0, 0 ],
-			[ 1, '1' ],
-			[ 2, object ],
-			[ 3, array ],
-			[ 4, foo ],
-			[ 5, undefined ]
-		]);
-		assert.deepEqual(map.values(), [
-			0, '1', object, array, foo, undefined
-		]);
-	},
-
-	entries: {
-		'empty map'() {
-			map = new Map<void, void>();
-			assert.deepEqual(map.entries(), []);
-		},
-
-		'nonempty map'() {
-			map = new Map<number, string>([
-				[ 1, 'a' ],
-				[ 2, 'b' ],
-				[ 3, 'c' ]
-			]);
-			assert.deepEqual(map.entries(), [
-				[ 1, 'a' ],
-				[ 2, 'b' ],
-				[ 3, 'c' ]
-			]);
-		}
-	},
-
-	iteration: {
-		before() {
-			function foo() {}
-			let object = Object.create(null);
-			let array = new Array();
-			mapArgs = [
-				[0, 0],
-				[1, '1'],
-				[2, object],
-				[3, array],
-				[4, foo],
-				[5, undefined]
-			]
-			map = new Map<number, any>(mapArgs);
-		},
-
-		'callback arguments'() {
-			map.forEach(function (key, value, mapInstance) {
-				assert.lengthOf(arguments, 3);
-				assert.strictEqual(map.get(key), value);
-				assert.strictEqual(map, mapInstance);
-			})
-		},
-
-		'times executed'() {
-			let counter = 0;
-			map.forEach(function (key, value, mapInstance) {
-				counter++;
-			});
-			assert.strictEqual(counter, mapArgs.length);
+			map.set('foo', 'baz');
+			assert.strictEqual(map.size, 1,
+				'size should remain the same after setting an existing key');
 		}
 	}
 });
