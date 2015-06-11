7837810e31b887e184f023b9c365be8ea687a817
Mangala SSS Khalsa <mkhalsa@sitepen.com>
2015-06-11 13:55:20 -0500

    Add EventedStreamSource stream apapter. Closes #20

--- tests/shim/unit/streams/all.ts
+++ tests/shim/unit/streams/all.ts
@@ -6,5 +6,6 @@ import './ReadableStreamReader';
 import './TransformStream';
 import './SizeQueue';
 import './WritableStream';
+import './adapters/EventedStreamSource';
 
 import 'dojo/has!host-node?./nodeOnly:./browserOnly';
