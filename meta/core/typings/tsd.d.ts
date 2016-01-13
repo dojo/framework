b3a73108380c26fd55cb989f88eec13ff986d6c9
Kitson Kelly <me@kitsonkelly.com>
2016-01-13 22:49:25 +0000

    Update dojo-loader.

--- meta/core/typings/tsd.d.ts
+++ meta/core/typings/tsd.d.ts
@@ -1,2 +1,2 @@
-/// <reference path="../node_modules/dojo-loader/typings/dojo-loader/dojo-loader-2.0.0-alpha.1.d.ts" />
+/// <reference path="../node_modules/dojo-loader/typings/dojo-loader/dojo-loader-2.0.0-alpha.4.d.ts" />
 /// <reference path="node/node.d.ts" />
