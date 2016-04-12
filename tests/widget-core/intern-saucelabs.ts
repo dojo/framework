92b532a6b24bbbe014ae5ce23c5edf6fc40ad330
Kitson Kelly <me@kitsonkelly.com>
2016-04-12 08:17:46 +0100

    Update dojo-core and remove unstable test platforms

--- tests/widget-core/intern-saucelabs.ts
+++ tests/widget-core/intern-saucelabs.ts
@@ -10,8 +10,8 @@ export const environments = [
 	// { browserName: 'safari', version: '8.0', platform: 'OS X 10.10' },
 	// { browserName: 'safari', version: '9.0', platform: 'OS X 10.11' },
 	{ browserName: 'android', deviceName: 'Google Nexus 7 HD Emulator' },
-	{ browserName: 'iphone', version: [ '7.1', '8.4' ] }
-	/* saucelabs has stability issues with iphone 9.1 and 9.2 */
+	{ browserName: 'iphone', version: '7.1' }
+	/* saucelabs has stability issues with iphone 8.4, 9.1 and 9.2 */
 ];
 
 /* SauceLabs supports more max concurrency */
