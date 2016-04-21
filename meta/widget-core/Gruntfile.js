d2d37ad7944fa576807992935b43fd0c8edf2839
Kitson Kelly <me@kitsonkelly.com>
2016-04-21 13:35:49 +0100

    Remove version number for distribution typings.
    Refs dojo/dojo2-package-template#27
    
    [ci skip]

--- meta/widget-core/Gruntfile.js
+++ meta/widget-core/Gruntfile.js
@@ -122,7 +122,7 @@ module.exports = function (grunt) {
 			},
 			dist: {
 				options: {
-					out: 'dist/typings/<%= name %>/<%= name %>-<%= version %>.d.ts'
+					out: 'dist/typings/<%= name %>/<%= name %>.d.ts'
 				},
 				src: [ '<%= skipTestsAndExamples %>' ]
 			}
