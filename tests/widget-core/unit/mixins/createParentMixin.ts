d4082c2653b1a12d2eab3098a4e6047c8ed2cde4
Kitson Kelly <me@kitsonkelly.com>
2016-05-24 12:16:49 +0100

    Add initial test for 'childlist' event

--- tests/widget-core/unit/mixins/createParentMixin.ts
+++ tests/widget-core/unit/mixins/createParentMixin.ts
@@ -1,6 +1,8 @@
 import * as registerSuite from 'intern!object';
 import * as assert from 'intern/chai!assert';
 import createParentMixin from 'src/mixins/createParentMixin';
+import createRenderable from 'src/mixins/createRenderable';
+import { List } from 'immutable/immutable';
 
 registerSuite({
 	name: 'mixins/createParentMixin',
@@ -9,5 +11,20 @@ registerSuite({
 		assert.isFunction(parent.append);
 		assert.isFunction(parent.insert);
 		assert.isObject(parent.children);
+	},
+	'on("childlist")': {
+		'append()'() {
+			const dfd = this.async();
+			const parent = createParentMixin();
+			const child = createRenderable();
+			parent.on('childlist', dfd.callback((event: any) => {
+				assert.strictEqual(event.type, 'childlist');
+				assert.strictEqual(event.target, parent);
+				assert.strictEqual(event.children, parent.children);
+				assert.isTrue(event.children.equals(List([ child ])));
+			}));
+
+			parent.append(child);
+		}
 	}
 });
