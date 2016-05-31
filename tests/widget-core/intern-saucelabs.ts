5bda7b1690cf3df49a5e1b34b6c987ad34954171
Kitson Kelly <me@kitsonkelly.com>
2016-05-31 09:12:47 +0100

    Update Intern

--- tests/widget-core/intern-saucelabs.ts
+++ tests/widget-core/intern-saucelabs.ts
@@ -2,16 +2,13 @@ export * from './intern';
 
 export const environments = [
 	{ browserName: 'internet explorer', version: [ '10.0', '11.0' ], platform: 'Windows 7' },
-	/* Having issues with Edge with Intern and SauceLabs */
-	// { browserName: 'MicrosoftEdge', platform: 'Windows 10' },
-	// { browserName: 'firefox', platform: 'Windows 10' },
+	{ browserName: 'MicrosoftEdge', platform: 'Windows 10' },
+	{ browserName: 'firefox', platform: 'Windows 10' },
 	{ browserName: 'chrome', platform: 'Windows 10' },
 	/* Having issues with Intern 3.1 and Safari on SauceLabs */
-	// { browserName: 'safari', version: '8.0', platform: 'OS X 10.10' },
-	// { browserName: 'safari', version: '9.0', platform: 'OS X 10.11' },
+	{ browserName: 'safari', version: '9.0', platform: 'OS X 10.11' },
 	{ browserName: 'android', deviceName: 'Google Nexus 7 HD Emulator' },
 	{ browserName: 'iphone', version: '7.1' }
-	/* saucelabs has stability issues with iphone 8.4, 9.1 and 9.2 */
 ];
 
 /* SauceLabs supports more max concurrency */
