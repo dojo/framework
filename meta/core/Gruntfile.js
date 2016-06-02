1fef232ba417fc8a282c12fa05f3703269b50303
Kitson Kelly <me@kitsonkelly.com>
2016-06-02 14:27:52 +0100

    Add main module for dts-generator.
    Fixes #166

--- meta/core/Gruntfile.js
+++ meta/core/Gruntfile.js
@@ -1,6 +1,9 @@
 module.exports = function (grunt) {
 	require('grunt-dojo2').initConfig(grunt, {
-		/* TODO: when this is updated in grunt-dojo2 */
-		staticTestFiles: [ 'tests/**/*.{html,css,json,xml,txt}' ]
+		dtsGenerator: {
+			options: {
+				main: 'dojo-core/main'
+			}
+		}
 	});
 };
