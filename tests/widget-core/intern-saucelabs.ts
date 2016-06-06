e374968b74e384f197c13850f7bd5e5499361fa4
Kitson Kelly <me@kitsonkelly.com>
2016-06-06 14:35:13 +0100

    Remove extra comment from intern-saucelabs

--- tests/widget-core/intern-saucelabs.ts
+++ tests/widget-core/intern-saucelabs.ts
@@ -5,7 +5,6 @@ export const environments = [
 	{ browserName: 'MicrosoftEdge', platform: 'Windows 10' },
 	{ browserName: 'firefox', platform: 'Windows 10' },
 	{ browserName: 'chrome', platform: 'Windows 10' },
-	/* Having issues with Intern 3.1 and Safari on SauceLabs */
 	{ browserName: 'safari', version: '9.0', platform: 'OS X 10.11' },
 	{ browserName: 'android', deviceName: 'Google Nexus 7 HD Emulator' },
 	{ browserName: 'iphone', version: '7.1' }
