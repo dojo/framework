7639bafdc5e54145136b7427b2fb71b8240f94f3
Kitson Kelly <me@kitsonkelly.com>
2016-07-01 13:54:46 +0100

    Switch back to Saucelabs

--- tests/shim/intern-saucelabs.ts
+++ tests/shim/intern-saucelabs.ts
@@ -4,7 +4,7 @@ export const environments = [
 	{ browserName: 'internet explorer', version: [ '10.0', '11.0' ], platform: 'Windows 7' },
 	/* Issues with SauceLabs and Edge ;-( */
 	// { browserName: 'microsoftedge', platform: 'Windows 10' },
-	{ browserName: 'firefox', platform: 'Windows 10' },
+	{ browserName: 'firefox', version: '43', platform: 'Windows 10' },
 	{ browserName: 'chrome', platform: 'Windows 10' },
 	/* Issues with SauceLabs and Safari ;-( */
 	// { browserName: 'safari', version: '9', platform: 'OS X 10.11' },
