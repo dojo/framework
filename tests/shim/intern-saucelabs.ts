544d96520757c6bc2057a1589ebd29315f257d49
Kitson Kelly <me@kitsonkelly.com>
2016-07-01 13:18:45 +0100

    Temp removal of Edge and Safari for SauceLabs

--- tests/shim/intern-saucelabs.ts
+++ tests/shim/intern-saucelabs.ts
@@ -2,10 +2,12 @@ export * from './intern';
 
 export const environments = [
 	{ browserName: 'internet explorer', version: [ '10.0', '11.0' ], platform: 'Windows 7' },
-	{ browserName: 'microsoftedge', platform: 'Windows 10' },
+	/* Issues with SauceLabs and Edge ;-( */
+	// { browserName: 'microsoftedge', platform: 'Windows 10' },
 	{ browserName: 'firefox', platform: 'Windows 10' },
 	{ browserName: 'chrome', platform: 'Windows 10' },
-	{ browserName: 'safari', version: '9', platform: 'OS X 10.11' },
+	/* Issues with SauceLabs and Safari ;-( */
+	// { browserName: 'safari', version: '9', platform: 'OS X 10.11' },
 	{ browserName: 'android', deviceName: 'Google Nexus 7 HD Emulator' },
 	{ browserName: 'iphone', version: '7.1' }
 ];
