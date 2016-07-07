97fea98ef2c652f54ef2c842643a3104e6c43fdc
Kitson Kelly <me@kitsonkelly.com>
2016-07-07 13:04:38 +0100

    Pin version of Firefox for SauceLabs

--- tests/routing/intern-saucelabs.ts
+++ tests/routing/intern-saucelabs.ts
@@ -2,13 +2,12 @@ export * from './intern';
 
 export const environments = [
 	{ browserName: 'internet explorer', version: [ '10.0', '11.0' ], platform: 'Windows 7' },
-	/* { browserName: 'microsoftedge', platform: 'Windows 10' }, */
-	{ browserName: 'firefox', platform: 'Windows 10' },
+	{ browserName: 'microsoftedge', platform: 'Windows 10' },
+	{ browserName: 'firefox', version: '43', platform: 'Windows 10' },
 	{ browserName: 'chrome', platform: 'Windows 10' },
-	/* Still appear to be issues on Safari on SauceLabs */
-	/* { browserName: 'safari', version: '9', platform: 'OS X 10.11' },*/
-	{ browserName: 'android', platform: 'Linux', version: '4.4', deviceName: 'Google Nexus 7 HD Emulator' }/*,
-	{ browserName: 'iphone', version: '9.1', deviceName: 'iPhone 6' }*/
+	{ browserName: 'safari', version: '9', platform: 'OS X 10.11' },
+	{ browserName: 'android', platform: 'Linux', version: '4.4', deviceName: 'Google Nexus 7 HD Emulator' },
+	{ browserName: 'iphone', version: '9.1', deviceName: 'iPhone 6' }
 ];
 
 /* SauceLabs supports more max concurrency */
