5d5e3b39ea6be9ac9d29b5e31e0d9fd128d585fe
Bryan Forbes <bryan@reigndropsfall.net>
2016-09-23 11:34:47 -0500

    Fixes #182 (#192)
    Improved the global object detection code

--- src/core/global.ts
+++ src/core/global.ts
@@ -1,2 +1,17 @@
-const globalObject: any = Function('return this')();
+const globalObject: any = (function (): any {
+	if (typeof window !== 'undefined') {
+		// Browsers
+		return window;
+	}
+	else if (typeof global !== 'undefined') {
+		// Node
+		return global;
+	}
+	else if (typeof self !== 'undefined') {
+		// Web workers
+		return self;
+	}
+	return {};
+})();
+
 export default globalObject;
