51d3aa3a8ae2e23d8e01dd7948fbf8cbad18228e
Mark Wubben <mark@novemberborn.net>
2016-09-26 17:21:11 +0100

    Promise callbacks always receive a value (#19)
    Fix optional typings for callback arguments, they'll always receive a
    value / reason. With the old typing strict null checks would always
    allow for the value to be undefined.

--- src/shim/Promise.ts
+++ src/shim/Promise.ts
@@ -280,8 +280,8 @@ export class PromiseShim<T> implements Thenable<T> {
 	private resolvedValue: any;
 
 	then: <U>(
-		onFulfilled?: (value?: T) => (U | Thenable<U>),
-		onRejected?: (reason?: Error) => (U | Thenable<U>)
+		onFulfilled?: (value: T) => (U | Thenable<U>),
+		onRejected?: (reason: Error) => (U | Thenable<U>)
 	) => PromiseShim<U>;
 
 	[Symbol.toStringTag]: string = 'Promise';
@@ -431,8 +431,8 @@ export default class Promise<T> implements Thenable<T> {
 	/**
 	 * Adds a callback to the promise to be invoked when the asynchronous operation throws an error.
 	 */
-	catch<U>(onRejected: (reason?: Error) => (U | Thenable<U>)): Promise<U>;
-	catch<U>(onRejected: (reason?: Error) => void): Promise<U> {
+	catch<U>(onRejected: (reason: Error) => (U | Thenable<U>)): Promise<U>;
+	catch<U>(onRejected: (reason: Error) => void): Promise<U> {
 		return this.then<U>(null, onRejected);
 	}
 
@@ -477,8 +477,8 @@ export default class Promise<T> implements Thenable<T> {
 	/**
 	 * Adds a callback to the promise to be invoked when the asynchronous operation completes successfully.
 	 */
-	then<U>(onFulfilled?: ((value?: T) => (U | Thenable<U> | null | undefined)) | null | undefined, onRejected?: (reason?: Error) => void): Promise<U>;
-	then<U>(onFulfilled?: ((value?: T) => (U | Thenable<U> | null | undefined)) | null | undefined, onRejected?: (reason?: Error) => (U | Thenable<U>)): Promise<U> {
+	then<U>(onFulfilled?: ((value: T) => (U | Thenable<U> | null | undefined)) | null | undefined, onRejected?: (reason: Error) => void): Promise<U>;
+	then<U>(onFulfilled?: ((value: T) => (U | Thenable<U> | null | undefined)) | null | undefined, onRejected?: (reason: Error) => (U | Thenable<U>)): Promise<U> {
 		return (<typeof Promise> this.constructor).copy(this.promise.then(onFulfilled, onRejected));
 	}
 }
--- src/shim/interfaces.ts
+++ src/shim/interfaces.ts
@@ -2,8 +2,8 @@
  * Thenable represents any object with a callable `then` property.
  */
 export interface Thenable<T> {
-	then<U>(onFulfilled?: (value?: T) => U | Thenable<U>, onRejected?: (error?: any) => U | Thenable<U>): Thenable<U>;
-	then<U>(onFulfilled?: (value?: T) => U | Thenable<U>, onRejected?: (error?: any) => void): Thenable<U>;
+	then<U>(onFulfilled?: (value: T) => U | Thenable<U>, onRejected?: (error: any) => U | Thenable<U>): Thenable<U>;
+	then<U>(onFulfilled?: (value: T) => U | Thenable<U>, onRejected?: (error: any) => void): Thenable<U>;
 }
 
 export interface ArrayLike<T> {
