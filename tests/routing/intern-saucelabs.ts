eb11ffc0bbd85ce76a6882559055bc7349fb0c8f
Mark Wubben <mark@novemberborn.net>
2016-10-19 10:41:11 +0100

    Update iOS version for tests

--- tests/routing/intern-saucelabs.ts
+++ tests/routing/intern-saucelabs.ts
@@ -7,7 +7,7 @@ export const environments = [
 	{ browserName: 'chrome', platform: 'Windows 10' },
 	{ browserName: 'safari', version: '9', platform: 'OS X 10.11' },
 	{ browserName: 'android', deviceName: 'Google Nexus 7 HD Emulator' },
-	{ browserName: 'iphone', version: '7.1' }
+	{ browserName: 'iphone', version: '9.3' }
 ];
 
 /* SauceLabs supports more max concurrency */
