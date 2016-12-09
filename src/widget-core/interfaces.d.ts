bae00d5f9c0638c760b828ed0abf5f2f5889e037
Anthony Gubler <agubler@gmail.com>
2016-12-09 17:06:40 +0000

    remove unused interfaces (#172)

--- src/widget-core/interfaces.d.ts
+++ src/widget-core/interfaces.d.ts
@@ -1,21 +1,13 @@
 /**
- * These represent the base classes for widgets, which have base implementations.
- *
- * These bases though will likely have signficant cross cutting concerns and therefore are located here.
+ * These represent the base classes and capabilties for widgets.
  *
  * Additional features and functionality are added to widgets by compositing mixins onto these
  * bases.
- *
- * The main interfaces that are defined in this module are:
- *   - Widget
- *   - ContainerWidget
  */
 
 import Promise from 'dojo-shim/Promise';
-import Map from 'dojo-shim/Map';
-import { Renderable, RenderableParent } from 'dojo-interfaces/abilities';
 import { EventedListener, State, Stateful, StatefulOptions } from 'dojo-interfaces/bases';
-import { EventTargettedObject, Factory, Handle, StylesMap } from 'dojo-interfaces/core';
+import { EventTargettedObject, Handle, StylesMap } from 'dojo-interfaces/core';
 import { VNode, VNodeProperties } from 'dojo-interfaces/vdom';
 import { ComposeFactory } from 'dojo-compose/compose';
 import { VNodeEvented, VNodeEventedOptions } from './mixins/createVNodeEvented';
@@ -37,9 +29,6 @@ export interface ChildNodeFunction {
 /**
  * A function that is called when collecting the node attributes on render, accepting the current map of
  * attributes and returning a set of VNode properties that should mixed into the current attributes.
- *
- * TODO: Should this act more like an actualy reducer, where the previousValue is passed in and is mutated directly,
- *       along with the instance reference?  Something like (previousAttributes: VNodeProperties, instance: WidgetMixin): VNodeProperties
  */
 export interface NodeAttributeFunction {
 	/**
@@ -48,185 +37,6 @@ export interface NodeAttributeFunction {
 	 * @param attributes The current VNodeProperties that will be part of the render
 	 */
 	(this: Widget<WidgetState>, attributes: VNodeProperties): VNodeProperties;
-}
-
-export interface ChildrenChangeEvent<T> {
-	/**
-	 * The subject of the event
-	 */
-	target: T;
-
-	/**
-	 * The type of the event
-	 */
-	type: 'children:changed';
-}
-
-export interface ContainerWidgetMixin<C extends Renderable> {
-	/**
-	 * A map of children *owned* by the widget
-	 */
-	readonly children: Map<string, C>;
-
-	/**
-	 * Remove all the children from the widget, but do not destroy them
-	 */
-	clear(): void;
-
-	/**
-	 * Create a list or map of children and add them to the children owned by this widget
-	 *
-	 * @param children A tuple list of children made up of the factory and the options to pass to the factory or a map of
-	 *                 children to be created
-	 */
-	createChildren<O extends WidgetOptions<WidgetState>>(children: CreateWidgetList<C, O> | CreateWidgetMap<C, O>): Promise<CreateWidgetResults<C>>;
-
-	/**
-	 * Set a child to the end of the children associated with this widget, using the `child.id` as the label for the
-	 * child
-	 *
-	 * @param child The child to set
-	 */
-	set(child: C | C[] | { [label: string]: C }): Handle;
-
-	/**
-	 * Set a child to the end of the children associated with this widget, using the supplied string as the label for
-	 * the child.
-	 *
-	 * @param label The label for the child
-	 * @param child The child to set
-	 */
-	set(label: string, child: C): Handle;
-
-	/**
-	 * Called (if present) when children are rendered to ensure that the children are rendered in the correct order.
-	 *
-	 * If the function returns `-1` then `childA` comes before `childB`, if `0` is returned the order remains unchanged, and
-	 * if `1` is returned `childB` comes before `childA`.
-	 *
-	 * @param childA The first child to compare
-	 * @param childB The second child to compare
-	 */
-	sort?(childA: C, childB: C): 0 | 1 | -1;
-}
-
-export interface ContainerWidgetOverloads<C extends Renderable> {
-	/**
-	 * Add a listener to the `children:changed` event which is fired when there is a change in the children of the widget
-	 *
-	 * @param type The event type to listen for
-	 * @param listener The event listener which will be called when the event is emitted
-	 */
-	on(type: 'children:changed', listener: EventedListener<
-		ContainerWidget<C, ContainerWidgetState>,
-		ChildrenChangeEvent<ContainerWidget<C, ContainerWidgetState>>
-	>): Handle;
-}
-
-/**
- * The *final* type for ContainerWidget
- */
-export type ContainerWidget<C extends Renderable, S extends ContainerWidgetState> = Widget<S> & ContainerWidgetMixin<C> & ContainerWidgetOverloads<C>;
-
-export interface ContainerWidgetOptions<C extends Renderable, S extends ContainerWidgetState> extends WidgetOptions<S> {
-	/**
-	 * A list of children to be created and added to the container at creation time
-	 */
-	createChildren?: CreateWidgetList<C, WidgetOptions<WidgetState>> | CreateWidgetMap<C, WidgetOptions<WidgetState>>;
-
-	/**
-	 * Provide a sort function for this instance which will sort the children renders at render time
-	 */
-	sort?(childA: C, childB: C): 0 | 1 | -1;
-}
-
-export interface ContainerWidgetState extends WidgetState {
-	/**
-	 * The IDs of the children which are currently owned by the containing widget
-	 */
-	children: string[];
-}
-
-/**
- * A set of widget factories and options used to create new widgets
- */
-export type CreateWidgetList<W extends Renderable, O extends WidgetOptions<WidgetState>> = ([Factory<W, O>, O ] | [Factory<W, O>])[];
-
-/**
- * A map of widget factories and options, where the key of the map is used as the label for the created child
- */
-export interface CreateWidgetMap<W extends Renderable, O extends WidgetOptions<WidgetState>> {
-	[label: string]: {
-		factory: Factory<W, O>;
-		options?: O;
-	};
-}
-
-export interface CreateWidgetOptions<C extends Renderable, O extends WidgetOptions<WidgetState>> {
-	/**
-	 * The factory to use in creating the child
-	 */
-	factory: Factory<C, O>;
-
-	/**
-	 * The label to assign the child to, if omitted, the child's `.id` property will be used
-	 */
-	label?: string;
-
-	/**
-	 * Any options to pass to the factory when creating the child
-	 */
-	options?: O;
-}
-
-/**
- * Interface that provides a map of widgets, used when returning from a creation API
- */
-export interface CreateWidgetResults<W extends Renderable> {
-	[label: string]: W;
-}
-
-export interface SubWidgetManager<W extends Renderable> {
-	/**
-	 * Create an instance of a widget based on the a passed factory and add it to the widgets directly managed
-	 * by the composite widget
-	 *
-	 * @param label The label which the widget should be referenced by
-	 * @param factory The factory which will return an instance when called
-	 * @param options Any options to pass to the factory upon construction
-	 */
-	create<V extends W,  O extends WidgetOptions<WidgetState>>(options: CreateWidgetOptions<V, O>): Promise<[string, V]>;
-
-	/**
-	 * Create instances of widgets based on the passed map of widget factories which are passed
-	 *
-	 * @param widgetFactories A map where the key is the label and the value is an object which provides the factory and
-	 *                        any options that should be passed to the constructor
-	 */
-	create<O extends WidgetOptions<WidgetState>>(widgetFactories: CreateWidgetMap<W, O>): Promise<CreateWidgetResults<W>>;
-
-	/**
-	 * Retrieve an instance of a widget which is part of the composite widget
-	 */
-	get<V extends W>(label: string): V | undefined;
-
-	/**
-	 * Returns `true` if the label is currently registered with the composite widget, otherwise returns `false`
-	 */
-	has(label: string): boolean;
-
-	/**
-	 * Adds a sub widget to the widgets directly managed by the composite widget
-	 *
-	 * @param label The label which the widget should be referenced by
-	 * @param widget The instance of the widget to add
-	 */
-	set(label: string, widget: W): Handle;
-
-	/**
-	 * Returns the number of widgets that are being directly managed by the composite widget
-	 */
-	readonly size: number;
 }
 
 export type WidgetFactoryFunction = () => Promise<WidgetFactory>
@@ -390,11 +200,6 @@ export interface WidgetOptions<S extends WidgetState> extends StatefulOptions<S>
 	 * Any node attribute functions that should be added to this instance
 	 */
 	nodeAttributes?: NodeAttributeFunction | NodeAttributeFunction[];
-
-	/**
-	 * A parent to assign to this widget at creation time
-	 */
-	parent?: RenderableParent;
 
 	/**
 	 * Override the tag name for this widget instance
