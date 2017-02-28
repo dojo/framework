5c6192f0e0bbabb1b621f006f1da3b5c04a7f367
Anthony Gubler <agubler@gmail.com>
2017-02-28 08:02:12 +0000

    remove android due to saucelabs issues and IE10 as not supported (#297)

--- tests/core/intern-saucelabs.ts
+++ tests/core/intern-saucelabs.ts
@@ -7,12 +7,12 @@ export const capabilities = {
 };
 
 export const environments = [
-	{ browserName: 'internet explorer', version: [ '10.0', '11.0' ], platform: 'Windows 7' },
+	{ browserName: 'internet explorer', version: [ '11.0' ], platform: 'Windows 7' },
 	// { browserName: 'microsoftedge', platform: 'Windows 10' },
 	{ browserName: 'firefox', version: '43', platform: 'Windows 10' },
-	{ browserName: 'chrome', platform: 'Windows 10' },
+	{ browserName: 'chrome', platform: 'Windows 10' }
 	// { browserName: 'safari', version: '9', platform: 'OS X 10.11' },
-	{ browserName: 'android', platform: 'Linux', version: '4.4', deviceName: 'Google Nexus 7 HD Emulator' }// ,
+	// { browserName: 'android', platform: 'Linux', version: '4.4', deviceName: 'Google Nexus 7 HD Emulator' }// ,
 	// { browserName: 'iphone', version: '9.1', deviceName: 'iPhone 6' }
 ];
 
