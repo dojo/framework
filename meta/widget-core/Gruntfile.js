19a8b8f66af520d02d3c321a7f48a25d75287b11
Paul Shannon <pshannon@sitepen.com>
2017-03-16 17:19:34 -0700

    Added grunt doc and keys for automated API builds

--- meta/widget-core/Gruntfile.js
+++ meta/widget-core/Gruntfile.js
@@ -1,3 +1,9 @@
 module.exports = function (grunt) {
-	require('grunt-dojo2').initConfig(grunt, {});
+	require('grunt-dojo2').initConfig(grunt, {
+		typedoc: {
+			options: {
+				ignoreCompilerErrors: true // Remove this once compile errors are resolved
+			}
+		}
+	});
 };
