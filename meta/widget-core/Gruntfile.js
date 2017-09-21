a74db85487fe89800c241bd627d274b00ce415fd
Anthony Gubler <agubler@gmail.com>
2017-09-21 12:01:56 +0100

    exclude tsx test files (#691)

--- meta/widget-core/Gruntfile.js
+++ meta/widget-core/Gruntfile.js
@@ -1,5 +1,10 @@
 module.exports = function (grunt) {
 	require('grunt-dojo2').initConfig(grunt, {
+		ts: {
+			dist: {
+				exclude: [ 'tests/**/*.tsx', 'tests/**/*.ts' ]
+			}
+		},
 		typedoc: {
 			options: {
 				ignoreCompilerErrors: true // Remove this once compile errors are resolved
