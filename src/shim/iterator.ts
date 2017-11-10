5c4004bf2eee1ec1c8994165fcf0dd15aa2d0296
rorticus <rorticus@gmail.com>
2017-11-10 15:44:56 -0500

    Re-adding forOf to unbreak other packages (#119)
    * Re-adding forOf to unbreak other packages

--- src/shim/iterator.ts
+++ src/shim/iterator.ts
@@ -1,4 +1,5 @@
 import './Symbol';
+import { HIGH_SURROGATE_MAX, HIGH_SURROGATE_MIN } from './string';
 
 export interface IteratorResult<T> {
 	readonly done: boolean;
@@ -80,4 +81,76 @@ export function isIterable(value: any): value is Iterable<any> {
  */
 export function isArrayLike(value: any): value is ArrayLike<any> {
 	return value && typeof value.length === 'number';
+}
+
+/**
+ * Returns the iterator for an object
+ *
+ * @param iterable The iterable object to return the iterator for
+ */
+export function get<T>(iterable: Iterable<T> | ArrayLike<T>): Iterator<T> | undefined {
+	if (isIterable(iterable)) {
+		return iterable[Symbol.iterator]();
+	}
+	else if (isArrayLike(iterable)) {
+		return new ShimIterator(iterable);
+	}
+};
+
+export interface ForOfCallback<T> {
+	/**
+	 * A callback function for a forOf() iteration
+	 *
+	 * @param value The current value
+	 * @param object The object being iterated over
+	 * @param doBreak A function, if called, will stop the iteration
+	 */
+	(value: T, object: Iterable<T> | ArrayLike<T> | string, doBreak: () => void): void;
+}
+
+/**
+ * Shims the functionality of `for ... of` blocks
+ *
+ * @param iterable The object the provides an interator interface
+ * @param callback The callback which will be called for each item of the iterable
+ * @param thisArg Optional scope to pass the callback
+ */
+export function forOf<T>(iterable: Iterable<T> | ArrayLike<T> | string, callback: ForOfCallback<T>, thisArg?: any): void {
+	let broken = false;
+
+	function doBreak() {
+		broken = true;
+	}
+
+	/* We need to handle iteration of double byte strings properly */
+	if (isArrayLike(iterable) && typeof iterable === 'string') {
+		const l = iterable.length;
+		for (let i = 0; i < l; ++i) {
+			let char = iterable[i];
+			if ((i + 1) < l) {
+				const code = char.charCodeAt(0);
+				if ((code >= HIGH_SURROGATE_MIN) && (code <= HIGH_SURROGATE_MAX)) {
+					char += iterable[++i];
+				}
+			}
+			callback.call(thisArg, char, iterable, doBreak);
+			if (broken) {
+				return;
+			}
+		}
+	}
+	else {
+		const iterator = get(iterable);
+		if (iterator) {
+			let result = iterator.next();
+
+			while (!result.done) {
+				callback.call(thisArg, result.value, iterable, doBreak);
+				if (broken) {
+					return;
+				}
+				result = iterator.next();
+			}
+		}
+	}
 }
--- tests/shim/unit/iterator.ts
+++ tests/shim/unit/iterator.ts
@@ -1,4 +1,4 @@
-import { ShimIterator } from '../../src/iterator';
+import { forOf, get, ShimIterator } from '../../src/iterator';
 import '../../src/Symbol';
 
 const { registerSuite } = intern.getInterface('object');
@@ -6,7 +6,6 @@ const { assert } = intern.getPlugin('chai');
 
 registerSuite('iterator', {
 	'for..of': {
-
 		'strings'() {
 			const str = 'abcdefg';
 			const results: string[] = [];
@@ -84,6 +83,206 @@ registerSuite('iterator', {
 
 			assert.deepEqual(results, expected);
 		}
+	},
+	'forOf': {
+
+		'strings'() {
+			const str = 'abcdefg';
+			const results: string[] = [];
+			const expected = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g' ];
+
+			forOf(str, (value) => {
+				results.push(value);
+			});
+
+			assert.deepEqual(results, expected);
+		},
+
+		'double byte strings'() {
+			const str = '¯\\_(ツ)_/¯';
+			const results: string[] = [];
+			const expected = [ '¯', '\\', '_', '(', 'ツ', ')', '_', '/', '¯' ];
+
+			forOf(str, (value) => {
+				results.push(value);
+			});
+
+			assert.deepEqual(results, expected);
+		},
+
+		'utf-16'() {
+			const str = '\uD801\uDC00';
+			const results: string[] = [];
+			const expected = [ '\uD801\uDC00' ];
+
+			forOf(str, (value) => {
+				results.push(value);
+			});
+
+			assert.deepEqual(results, expected);
+		},
+
+		'arrays'() {
+			const array: any[] = [ 'foo', 'bar', {}, 1, [ 'qat', 2 ] ];
+			const results: any[] = [];
+
+			forOf(array, (value) => {
+				results.push(value);
+			});
+
+			assert.deepEqual(results, array);
+			assert.notStrictEqual(results, array);
+		},
+
+		'iterable'() {
+			let counter = 0;
+			const iterable = {
+				[Symbol.iterator]() {
+					return {
+						next(): {value: number | undefined, done: boolean}  {
+							counter++;
+							if (counter < 5) {
+								return {
+									value: counter,
+									done: false
+								};
+							}
+							else {
+								return { done: true, value: undefined };
+							}
+						}
+					};
+				}
+			};
+			const results: (undefined | number)[] = [];
+			const expected = [ 1, 2, 3, 4 ];
+
+			forOf(iterable, (value) => {
+				results.push(value);
+			});
+
+			assert.deepEqual(results, expected);
+		},
+
+		'scoping'() {
+			const array = [ 'a', 'b' ];
+			const scope = { foo: 'bar' };
+
+			forOf(array, function (this: any, value: any, obj: any) {
+				assert.strictEqual(this, scope);
+				assert.strictEqual(obj, array);
+			}, scope);
+		},
+
+		'doBreak': {
+			strings() {
+				const str = 'abcdefg';
+				let counter = 0;
+				const results: string[] = [];
+				const expected = [ 'a', 'b', 'c' ];
+
+				forOf(str, (value, str, doBreak) => {
+					results.push(value);
+					counter++;
+					if (counter >= 3) {
+						doBreak();
+					}
+				});
+
+				assert.deepEqual(results, expected);
+			},
+			iterable() {
+				let counter = 0;
+				const iterable = {
+					[Symbol.iterator]() {
+						return {
+							next(): {value: number | undefined, done: boolean} {
+								counter++;
+								if (counter < 5) {
+									return {
+										value: counter,
+										done: false
+									};
+								}
+								else {
+									return { done: true, value: undefined };
+								}
+							}
+						};
+					}
+				};
+				const results: (undefined | number)[] = [];
+				const expected = [ 1, 2 ];
+
+				forOf(iterable, (value, obj, doBreak) => {
+					results.push(value);
+					if (value === 2) {
+						doBreak();
+					}
+				});
+
+				assert.deepEqual(results, expected);
+			}
+		}
+
+	},
+
+	'get': {
+
+		'iterable'() {
+			let counter = 0;
+			const iterable = {
+				[Symbol.iterator]() {
+					return {
+						next(): {value: number | undefined, done: boolean} {
+							counter++;
+							if (counter < 5) {
+								return {
+									value: counter,
+									done: false
+								};
+							}
+							else {
+								return { done: true, value: undefined };
+							}
+						}
+					};
+				}
+			};
+			const results: (undefined | number)[] = [];
+			const expected = [ 1, 2, 3, 4 ];
+			const iterator = get(iterable);
+			if (iterator) {
+				let result = iterator.next();
+				while (!result.done) {
+					results.push(result.value);
+					result = iterator.next();
+				}
+			}
+			assert.deepEqual(results, expected);
+		},
+
+		'arrayLike'() {
+			const obj: { [idx: number]: number; length: number; } = {
+				0: 1,
+				1: 2,
+				2: 3,
+				3: 4,
+				length: 4
+			};
+			const results: number[] = [];
+			const expected = [ 1, 2, 3, 4 ];
+			const iterator = get(obj);
+			if (iterator) {
+				let result = iterator.next();
+				while (!result.done) {
+					results.push(result.value);
+					result = iterator.next();
+				}
+			}
+			assert.deepEqual(results, expected);
+		}
+
 	},
 
 	'class ShimIterator': {
