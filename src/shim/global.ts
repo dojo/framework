7b4fc730421074cab381e77d5e654473b7b9df25
Matthew Gadd <mgadd@sitepen.com>
2018-07-30 13:52:21 +0100

    check for window before global in global shim (#38)

--- src/shim/global.ts
+++ src/shim/global.ts
@@ -1,15 +1,15 @@
 const globalObject: any = (function(): any {
-	if (typeof global !== 'undefined') {
-		// global spec defines a reference to the global object called 'global'
-		// https://github.com/tc39/proposal-global
-		// `global` is also defined in NodeJS
-		return global;
-	} else if (typeof window !== 'undefined') {
-		// window is defined in browsers
-		return window;
-	} else if (typeof self !== 'undefined') {
-		// self is defined in WebWorkers
+	// the only reliable means to get the global object is
+	// `Function('return this')()`
+	// However, this causes CSP violations in Chrome apps.
+	if (typeof self !== 'undefined') {
 		return self;
+	}
+	if (typeof window !== 'undefined') {
+		return window;
+	}
+	if (typeof global !== 'undefined') {
+		return global;
 	}
 })();
 
