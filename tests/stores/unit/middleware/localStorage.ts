848e5dd1b3b7a182ef813b8c5e08121e191ebca1
Anthony Gubler <agubler@sitepen.com>
2018-08-14 12:03:45 +0100

    fix tests

--- tests/stores/unit/middleware/localStorage.ts
+++ tests/stores/unit/middleware/localStorage.ts
@@ -1,7 +1,6 @@
 import global from '../../../../src/shim/global';
 const { describe, it, beforeEach } = intern.getInterface('bdd');
 const { assert } = intern.getPlugin('chai');
-import { SinonStub, stub } from 'sinon';
 
 import { collector, load } from './../../../../src/stores/middleware/localStorage';
 import { Store } from './../../../../src/stores/Store';
@@ -14,18 +13,20 @@ function incrementCounter({ get, path }: CommandRequest<{ counter: number }>): P
 	return [{ op: OperationType.REPLACE, path: new Pointer('/counter'), value: ++counter }];
 }
 
-let getStub: SinonStub;
-let setStub: SinonStub;
+const LOCAL_STORAGE_TEST_ID = 'local-storage-id';
 
-if (global.localStorage) {
-	getStub = stub(global.localStorage, 'getItem');
-	setStub = stub(global.localStorage, 'setItem');
-} else {
-	getStub = stub();
-	setStub = stub();
+if (!global.localStorage) {
 	global.localStorage = {
-		getItem: getStub,
-		setItem: setStub
+		storage: {},
+		getItem(this: any, key: string) {
+			return this.storage[key];
+		},
+		setItem(this: any, key: string, item: string) {
+			this.storage[key] = item;
+		},
+		removeItem(this: any, key: string) {
+			delete this.storage[key];
+		}
 	};
 }
 
@@ -33,19 +34,21 @@ let store: Store;
 
 describe('middleware - local storage', () => {
 	beforeEach(() => {
+		global.localStorage.removeItem(LOCAL_STORAGE_TEST_ID);
 		store = new Store();
-		getStub.reset();
-		setStub.reset();
 	});
 
 	it('Should save state to local storage', () => {
 		const incrementCounterProcess = createProcess(
 			'increment',
 			[incrementCounter],
-			collector('local-storage-id', (path) => [path('counter')])
+			collector(LOCAL_STORAGE_TEST_ID, (path) => [path('counter')])
 		);
 		incrementCounterProcess(store)({});
-		assert.isTrue(setStub.calledWith('local-storage-id', '[{"meta":{"path":"/counter"},"state":1}]'));
+		assert.deepEqual(
+			global.localStorage.getItem(LOCAL_STORAGE_TEST_ID),
+			'[{"meta":{"path":"/counter"},"state":1}]'
+		);
 	});
 
 	it('Should call next middleware', () => {
@@ -54,7 +57,7 @@ describe('middleware - local storage', () => {
 			'increment',
 			[incrementCounter],
 			collector(
-				'local-storage-id',
+				LOCAL_STORAGE_TEST_ID,
 				(path) => [path('counter')],
 				(error, result) => {
 					composedMiddlewareCalled = true;
@@ -66,14 +69,14 @@ describe('middleware - local storage', () => {
 	});
 
 	it('should load from local storage', () => {
-		getStub.withArgs('local-storage-id').returns('[{"meta":{"path":"/counter"},"state":1}]');
-		load('local-storage-id', store);
+		global.localStorage.setItem(LOCAL_STORAGE_TEST_ID, '[{"meta":{"path":"/counter"},"state":1}]');
+		load(LOCAL_STORAGE_TEST_ID, store);
 		assert.deepEqual((store as any)._state, { counter: 1 });
 	});
 
 	it('should not load anything or throw an error if data does exist', () => {
-		getStub.withArgs('other-storage-id').returns('[{"meta":{"path":"/counter"},"state":1}]');
-		load('local-storage-id', store);
+		global.localStorage.setItem('other-storage-id', '[{"meta":{"path":"/counter"},"state":1}]');
+		load(LOCAL_STORAGE_TEST_ID, store);
 		assert.deepEqual((store as any)._state, {});
 	});
 });
