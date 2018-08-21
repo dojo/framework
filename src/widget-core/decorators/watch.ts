701d8c6d55b4c6a942c6019c6d7b37ab3fda3b78
Anthony Gubler <agubler@sitepen.com>
2018-08-21 11:13:33 +0100

    watch decorator

---
+++ src/widget-core/decorators/watch.ts
@@ -0,0 +1,20 @@
+import handleDecorator, { DecoratorHandler } from './handleDecorator';
+
+export function watch(): DecoratorHandler {
+	return handleDecorator((target, propertyKey) => {
+		target.addDecorator('afterConstructor', function(this: any) {
+			if (propertyKey) {
+				let _value: any = this[propertyKey];
+				Object.defineProperty(this, propertyKey, {
+					set(value: any) {
+						_value = value;
+						this.invalidate();
+					},
+					get() {
+						return _value;
+					}
+				});
+			}
+		});
+	});
+}
