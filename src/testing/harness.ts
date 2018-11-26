981a59e0b0ee7a41efd047c291acc740205f80f9
rorticus <rorticus@gmail.com>
2018-11-26 11:34:21 -0500

    Exposing select in harness (#162)
    * Exposing select in harness
    
    * Adding tryRender to select
    
    * Updating README

--- src/testing/harness.ts
+++ src/testing/harness.ts
@@ -42,11 +42,16 @@ export interface GetRender {
 	(index?: number): DNode | DNode[];
 }
 
+export interface Select {
+	(selector: string): DNode[];
+}
+
 export interface HarnessAPI {
 	expect: Expect;
 	expectPartial: ExpectPartial;
 	trigger: Trigger;
 	getRender: GetRender;
+	select: Select;
 }
 
 function decorateNodes(dNode: DNode[]): DecoratorResult<DNode[]>;
@@ -173,6 +178,10 @@ export function harness(
 		},
 		getRender(index?: number): DNode | DNode[] {
 			return _getRender(index);
+		},
+		select(selector: string): DNode[] {
+			_tryRender();
+			return select(selector, _getRender());
 		}
 	};
 }
