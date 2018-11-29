8941c0f1109d260765cc4c0fae989754a2639819
Anthony Gubler <agubler@gmail.com>
2018-11-29 14:28:36 +0000

    bind fetch to global (#192)

--- src/shim/fetch.ts
+++ src/shim/fetch.ts
@@ -2,4 +2,4 @@ import global from './global';
 `!has('fetch')`;
 import 'whatwg-fetch';
 
-export default global.fetch as (input: RequestInfo, init?: RequestInit) => Promise<Response>;
+export default global.fetch.bind(global) as (input: RequestInfo, init?: RequestInit) => Promise<Response>;
