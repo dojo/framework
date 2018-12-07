844f46f0a01fe15afac9fbabc606d16789c474f8
Anthony Gubler <agubler@sitepen.com>
2018-12-07 13:52:54 +0000

    Add typings for resize observer shim

--- src/shim/ResizeObserver.ts
+++ src/shim/ResizeObserver.ts
@@ -1,12 +1,40 @@
 import global from './global';
 import has from '../has/has';
 `!has('build-elide')`;
-import ResizeObserver from 'resize-observer-polyfill';
+import Resize from 'resize-observer-polyfill';
+
+export interface DOMRectReadOnly {
+	readonly x: number;
+	readonly y: number;
+	readonly width: number;
+	readonly height: number;
+	readonly top: number;
+	readonly right: number;
+	readonly bottom: number;
+	readonly left: number;
+}
+
+export interface ResizeObserverCallback {
+	(entries: ResizeObserverEntry[], observer: ResizeObserver): void;
+}
+
+export interface ResizeObserverEntry {
+	readonly target: Element;
+	readonly contentRect: DOMRectReadOnly;
+}
+
+export interface ResizeObserver {
+	prototype: ResizeObserver;
+	new (callback: ResizeObserverCallback): ResizeObserver;
+	observe(target: Element): void;
+	unobserve(target: Element): void;
+	disconnect(): void;
+}
 
 if (!has('build-elide')) {
 	if (!global.ResizeObserver) {
-		global.ResizeObserver = ResizeObserver;
+		global.ResizeObserver = Resize;
 	}
 }
 
-export default global.ResizeObserver as typeof ResizeObserver;
+export default global.ResizeObserver as ResizeObserver;
