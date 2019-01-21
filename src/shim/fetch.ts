ef4e5f06e38787bf7a0a58b74f6909eae7ba4c81
Anthony Gubler <agubler@gmail.com>
2019-01-21 17:17:09 +0000

    Elide fetch when build elide is on (#225)

--- src/shim/fetch.ts
+++ src/shim/fetch.ts
@@ -1,5 +1,5 @@
 import global from './global';
-`!has('fetch')`;
+`!has('build-elide')`;
 import 'whatwg-fetch';
 
 export default global.fetch.bind(global) as (input: RequestInfo, init?: RequestInit) => Promise<Response>;
