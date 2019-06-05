a0f0f0bb62b49581d084f47592dc69868d77e4b6
Anthony Gubler <agubler@sitepen.com>
2019-06-05 16:50:51 +0100

    Cache Middleware (#373)
    * Cache middleware
    
    * Add the middleware test import
    
    * Test clearing the cache

---
+++ src/core/middleware/cache.ts
@@ -0,0 +1,24 @@
+import { create, destroy } from '../vdom';
+import Map from '../../shim/Map';
+
+const factory = create({ destroy });
+
+export const cache = factory(({ middleware: { destroy } }) => {
+	const cacheMap = new Map<string, any>();
+	destroy(() => {
+		cacheMap.clear();
+	});
+	return {
+		get<T = any>(key: any): T | null {
+			return cacheMap.get(key);
+		},
+		set<T = any>(key: any, value: T): void {
+			cacheMap.set(key, value);
+		},
+		clear(): void {
+			cacheMap.clear();
+		}
+	};
+});
+
+export default cache;
