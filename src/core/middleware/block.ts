22de2aa7ed5272552d1816c1e6173a9e7a5d2efe
Anthony Gubler <agubler@sitepen.com>
2019-07-13 09:55:55 +0100

    Fix block function argument typings and update API

--- src/core/middleware/block.ts
+++ src/core/middleware/block.ts
@@ -6,22 +6,20 @@ const blockFactory = create({ defer, cache, icache });
 
 export const block = blockFactory(({ middleware: { cache, icache, defer } }) => {
 	let id = 1;
-	return {
-		run<T extends (...args: any[]) => any>(module: T) {
-			return (...args: any[]): (ReturnType<T> extends Promise<infer U> ? U : ReturnType<T>) | null => {
-				const argsString = JSON.stringify(args);
-				const moduleId = cache.get(module) || id++;
-				cache.set(module, moduleId);
-				const cachedValue = icache.getOrSet(`${moduleId}-${argsString}`, async () => {
-					const run = module(...args);
-					defer.pause();
-					const result = await run;
-					defer.resume();
-					return result;
-				});
-				return cachedValue || null;
-			};
-		}
+	return <T extends (...args: any[]) => any>(module: T) => {
+		return (...args: Parameters<T>): (ReturnType<T> extends Promise<infer U> ? U : ReturnType<T>) | null => {
+			const argsString = JSON.stringify(args);
+			const moduleId = cache.get(module) || id++;
+			cache.set(module, moduleId);
+			const cachedValue = icache.getOrSet(`${moduleId}-${argsString}`, async () => {
+				const run = module(...args);
+				defer.pause();
+				const result = await run;
+				defer.resume();
+				return result;
+			});
+			return cachedValue || null;
+		};
 	};
 });
 
