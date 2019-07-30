950a6b30f3fc3aeb6d388085f06f2ca390f5237e
Daniel Lautzenheiser <lautzd@gmail.com>
2019-07-30 11:41:30 -0400

    Improve return typing on icache getOrSet (#467)

--- src/core/middleware/icache.ts
+++ src/core/middleware/icache.ts
@@ -8,57 +8,70 @@ interface CacheWrapper<T = any> {
 	value: T;
 }
 
-export const icache = factory(({ middleware: { invalidator, cache } }) => {
-	return {
-		getOrSet<T = any>(key: any, value: any): T | undefined {
-			let cachedValue = cache.get<CacheWrapper<T>>(key);
-			if (!cachedValue) {
-				this.set(key, value);
-			}
-			cachedValue = cache.get<CacheWrapper<T>>(key);
-			if (!cachedValue || cachedValue.status === 'pending') {
-				return undefined;
-			}
-			return cachedValue.value;
-		},
-		get<T = any>(key: any): T | undefined {
-			const cachedValue = cache.get<CacheWrapper<T>>(key);
-			if (!cachedValue || cachedValue.status === 'pending') {
-				return undefined;
-			}
-			return cachedValue.value;
-		},
-		set(key: any, value: any): void {
-			if (typeof value === 'function') {
-				value = value();
-				if (value && typeof value.then === 'function') {
-					cache.set(key, {
-						status: 'pending',
-						value
-					});
-					value.then((result: any) => {
-						const cachedValue = cache.get<CacheWrapper<any>>(key);
-						if (cachedValue && cachedValue.value === value) {
-							cache.set(key, {
-								status: 'resolved',
-								value: result
-							});
-							invalidator();
-						}
-					});
-					return;
-				}
-			}
-			cache.set(key, {
-				status: 'resolved',
-				value
-			});
-			invalidator();
-		},
-		clear(): void {
-			cache.clear();
-		}
+export interface IcacheResult {
+	getOrSet: {
+		<T>(key: any, value: () => Promise<T>): undefined;
+		<T>(key: any, value: () => T): T;
+		<T>(key: any, value: T): T;
 	};
-});
+	get<T = any>(key: any): T | undefined;
+	set(key: any, value: any): void;
+	clear(): void;
+}
+
+export const icache = factory(
+	({ middleware: { invalidator, cache } }): IcacheResult => {
+		return {
+			getOrSet<T = any>(key: any, value: any): T | undefined {
+				let cachedValue = cache.get<CacheWrapper<T>>(key);
+				if (!cachedValue) {
+					this.set(key, value);
+				}
+				cachedValue = cache.get<CacheWrapper<T>>(key);
+				if (!cachedValue || cachedValue.status === 'pending') {
+					return undefined;
+				}
+				return cachedValue.value;
+			},
+			get<T = any>(key: any): T | undefined {
+				const cachedValue = cache.get<CacheWrapper<T>>(key);
+				if (!cachedValue || cachedValue.status === 'pending') {
+					return undefined;
+				}
+				return cachedValue.value;
+			},
+			set(key: any, value: any): void {
+				if (typeof value === 'function') {
+					value = value();
+					if (value && typeof value.then === 'function') {
+						cache.set(key, {
+							status: 'pending',
+							value
+						});
+						value.then((result: any) => {
+							const cachedValue = cache.get<CacheWrapper<any>>(key);
+							if (cachedValue && cachedValue.value === value) {
+								cache.set(key, {
+									status: 'resolved',
+									value: result
+								});
+								invalidator();
+							}
+						});
+						return;
+					}
+				}
+				cache.set(key, {
+					status: 'resolved',
+					value
+				});
+				invalidator();
+			},
+			clear(): void {
+				cache.clear();
+			}
+		};
+	}
+);
 
 export default icache;
