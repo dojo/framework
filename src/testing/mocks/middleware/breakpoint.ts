aef3f8963411a5df8be0630bdf1f8c40afa7e487
Bradley Maier <bmaier@sitepen.com>
2019-07-30 12:08:05 -0400

    Add breakpoint middleware mock (#471)

---
+++ src/testing/mocks/middleware/breakpoint.ts
@@ -0,0 +1,66 @@
+import { create, destroy, invalidator, node } from '../../../core/vdom';
+import resize from '../../../core/middleware/resize';
+import icache from '../../../core/middleware/icache';
+import breakpoint, { Breakpoints } from '../../../core/middleware/breakpoint';
+import createResizeMock from './resize';
+import { MiddlewareResult } from '../../../core/interfaces';
+
+export function createBreakpointMock(breakpoints: Breakpoints = { SM: 0, MD: 576, LG: 768, XL: 960 }) {
+	const mockBreakpoints: any = {};
+	const defaultBreakpoints = breakpoints;
+	const resizeMockFactory = createResizeMock();
+	const factory = create({ resize, node, destroy, icache, invalidator });
+
+	const mockBreakpointFactory = factory((payload) => {
+		const { id, properties, children } = payload;
+		const { callback } = breakpoint();
+		const mock = callback({
+			id,
+			middleware: { resize: resizeMockFactory().callback(payload) },
+			properties,
+			children
+		});
+
+		return {
+			get(key: string | number, breakpoints: Breakpoints = defaultBreakpoints) {
+				const result = mock.get(key, breakpoints);
+				if (mockBreakpoints[key]) {
+					return {
+						breakpoint: mockBreakpoints[key].breakpoint || (result && result.breakpoint),
+						contentRect: result && result.contentRect
+					};
+				}
+				return null;
+			}
+		};
+	});
+
+	function mockBreakpoint(): MiddlewareResult<any, any, any>;
+	function mockBreakpoint(
+		key: string,
+		breakpointResult: {
+			breakpoint: string;
+			contentRect: Partial<DOMRectReadOnly>;
+		}
+	): void;
+	function mockBreakpoint(
+		key?: string,
+		breakpointResult?: {
+			breakpoint: string;
+			contentRect: Partial<DOMRectReadOnly>;
+		}
+	): void | MiddlewareResult<any, any, any> {
+		if (key && breakpointResult) {
+			if (!mockBreakpoints[key]) {
+				mockBreakpoints[key] = breakpointResult.breakpoint;
+			}
+			resizeMockFactory(key, breakpointResult.contentRect);
+		} else {
+			return mockBreakpointFactory();
+		}
+	}
+
+	return mockBreakpoint;
+}
+
+export default createBreakpointMock;
