a8629d9acb3f54d5365bb1d52997babe7a9f838d
Bradley Maier <bmaier@sitepen.com>
2019-07-30 13:28:27 -0400

    Use indexOf instead of includes

--- src/shim/global.ts
+++ src/shim/global.ts
@@ -2,7 +2,7 @@ const globalObject: any = (function(): any {
 	// the only reliable means to get the global object is
 	// `Function('return this')()`
 	// However, this causes CSP violations in Chrome apps.
-	if (typeof window !== 'undefined' && window.navigator.userAgent.includes('jsdom')) {
+	if (typeof window !== 'undefined' && window.navigator.userAgent.indexOf('jsdom') > -1) {
 		return window;
 	}
 	if (typeof globalThis !== 'undefined') {
