b6fe206391d92a07db89cf1dfd1866daa660f6b7
Anthony Gubler <agubler@sitepen.com>
2019-08-31 13:21:31 +0100

    Expose factory for typing icache to an interface

--- src/core/middleware/icache.ts
+++ src/core/middleware/icache.ts
@@ -3,75 +3,94 @@ import cache from './cache';
 
 const factory = create({ cache, invalidator });
 
-interface CacheWrapper<T = any> {
+interface CacheWrapper {
 	status: 'pending' | 'resolved';
-	value: T;
+	value: any;
 }
 
-export interface IcacheResult {
+export interface ICacheResult<S = null> {
 	getOrSet: {
-		<T>(key: any, value: () => Promise<T>): undefined | T;
-		<T>(key: any, value: () => T): T;
-		<T>(key: any, value: T): T;
+		<T extends null extends S ? any : keyof S>(
+			key: null extends S ? any : T,
+			value: null extends S ? () => Promise<T> : () => Promise<S[T]>
+		): null extends S ? undefined | T : undefined | S[T];
+		<T extends null extends S ? any : keyof S>(
+			key: null extends S ? any : T,
+			value: null extends S ? () => T : () => S[T]
+		): null extends S ? T : S[T];
+		<T extends null extends S ? any : keyof S>(
+			key: null extends S ? any : T,
+			value: null extends S ? T : S[T]
+		): null extends S ? T : S[T];
 	};
-	get<T = any>(key: any): T | undefined;
-	set(key: any, value: any): void;
+	get<T extends null extends S ? any : keyof S>(
+		key: null extends S ? any : T
+	): null extends S ? T | undefined : S[T] | undefined;
+	set<T extends null extends S ? any : keyof S>(
+		key: null extends S ? any : T,
+		value: null extends S ? T : S[T]
+	): void;
 	clear(): void;
 }
 
-export const icache = factory(
-	({ middleware: { invalidator, cache } }): IcacheResult => {
-		return {
-			getOrSet<T = any>(key: any, value: any): T | undefined {
-				let cachedValue = cache.get<CacheWrapper<T>>(key);
-				if (!cachedValue) {
-					this.set(key, value);
-				}
-				cachedValue = cache.get<CacheWrapper<T>>(key);
-				if (!cachedValue || cachedValue.status === 'pending') {
-					return undefined;
-				}
-				return cachedValue.value;
-			},
-			get<T = any>(key: any): T | undefined {
-				const cachedValue = cache.get<CacheWrapper<T>>(key);
-				if (!cachedValue || cachedValue.status === 'pending') {
-					return undefined;
-				}
-				return cachedValue.value;
-			},
-			set(key: any, value: any): void {
-				if (typeof value === 'function') {
-					value = value();
-					if (value && typeof value.then === 'function') {
-						cache.set(key, {
-							status: 'pending',
-							value
-						});
-						value.then((result: any) => {
-							const cachedValue = cache.get<CacheWrapper<any>>(key);
-							if (cachedValue && cachedValue.value === value) {
-								cache.set(key, {
-									status: 'resolved',
-									value: result
-								});
-								invalidator();
-							}
-						});
-						return;
+export function createICacheMiddleware<S = any>() {
+	const icache = factory(
+		({ middleware: { invalidator, cache } }): ICacheResult<S> => {
+			return {
+				getOrSet(key: any, value: any): any | undefined {
+					let cachedValue = cache.get<CacheWrapper>(key);
+					if (!cachedValue) {
+						this.set(key, value);
 					}
+					cachedValue = cache.get<CacheWrapper>(key);
+					if (!cachedValue || cachedValue.status === 'pending') {
+						return undefined;
+					}
+					return cachedValue.value;
+				},
+				get(key: any): any {
+					const cachedValue = cache.get<CacheWrapper>(key);
+					if (!cachedValue || cachedValue.status === 'pending') {
+						return undefined;
+					}
+					return cachedValue.value;
+				},
+				set(key: any, value: any): void {
+					if (typeof value === 'function') {
+						value = value();
+						if (value && typeof value.then === 'function') {
+							cache.set(key, {
+								status: 'pending',
+								value
+							});
+							value.then((result: any) => {
+								const cachedValue = cache.get<CacheWrapper>(key);
+								if (cachedValue && cachedValue.value === value) {
+									cache.set(key, {
+										status: 'resolved',
+										value: result
+									});
+									invalidator();
+								}
+							});
+							return;
+						}
+					}
+					cache.set(key, {
+						status: 'resolved',
+						value
+					});
+					invalidator();
+				},
+				clear(): void {
+					cache.clear();
 				}
-				cache.set(key, {
-					status: 'resolved',
-					value
-				});
-				invalidator();
-			},
-			clear(): void {
-				cache.clear();
-			}
-		};
-	}
-);
+			};
+		}
+	);
+	return icache;
+}
+
+const icache = createICacheMiddleware();
 
 export default icache;
