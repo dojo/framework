2578bef66a33dbc5ad047982d6b8c82a29f33832
Anthony Gubler <agubler@sitepen.com>
2019-12-21 17:55:08 +0000

    Add warning about cache deprecation in debug mode

--- src/core/middleware/cache.ts
+++ src/core/middleware/cache.ts
@@ -1,9 +1,15 @@
+import has from './../has';
 import { create } from '../vdom';
 import icache from './icache';
 
 const factory = create({ icache });
 
 export const cache = factory(({ middleware: { icache } }) => {
+	if (has('dojo-debug')) {
+		console.warn(
+			'The cache middleware has been deprecated. Please use the icache middleware instead, for details please see the documentation https://dojo.io/learn/middleware/available-middleware#icache'
+		);
+	}
 	return {
 		get<T = any>(key: any): T | undefined {
 			return icache.get(key);
