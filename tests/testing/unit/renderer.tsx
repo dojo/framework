d903782ec528a31ed1e6b6499c514b01615c4bc9
Bradley Maier <bmaier@sitepen.com>
2020-09-09 11:11:08 -0700

    Handle ignored children in decorate (#840)

--- tests/testing/unit/renderer.tsx
+++ tests/testing/unit/renderer.tsx
@@ -14,6 +14,23 @@ const noop: any = () => {};
 
 class ChildWidget extends WidgetBase<{ id: string; func?: () => void }> {}
 
+const ConditionalRender = create({ icache })(({ middleware: { icache } }) => {
+	return v('div', {}, [
+		icache.get('render')
+			? v('div', {
+					onclick: () => {
+						icache.set('render', false);
+					}
+			  })
+			: null,
+		v('div', {
+			onclick: () => {
+				icache.set('render', true);
+			}
+		})
+	]);
+});
+
 class MyWidget extends WidgetBase {
 	_count = 0;
 	_result = 'result';
@@ -220,6 +237,19 @@ describe('test renderer', () => {
 					])
 				)
 			);
+		});
+
+		it('triggers property when there are undefined children in actual render', () => {
+			const WrappedRoot = wrap('div');
+			const WrappedChild = wrap('div');
+			const WrappedConditional = wrap('div');
+			const baseTemplate = assertion(() => v(WrappedRoot.tag, {}, [v(WrappedChild.tag, { onclick: noop })]));
+			const r = renderer(() => w(ConditionalRender, {}));
+			r.expect(baseTemplate);
+			r.property(WrappedChild, 'onclick');
+			r.expect(baseTemplate.prepend(WrappedRoot, () => [v(WrappedConditional.tag, { onclick: noop })]));
+			r.property(WrappedConditional, 'onclick');
+			r.expect(baseTemplate);
 		});
 
 		it('should call properties in the correct order', () => {
